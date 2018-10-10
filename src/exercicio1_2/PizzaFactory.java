64364e8c4f7ecde8e341ca32b2f591cb54bb3d75
Lucca Machado <lucca.santos@acad.pucrs.br>
2018-10-10 16:00:06 -0300

    adição do joda time, assim como os testes em todos os casos possíveis.

--- src/exercicio1_2/PizzaFactory.java
+++ src/exercicio1_2/PizzaFactory.java
@@ -1,6 +1,8 @@
 package exercicio1_2;
 
-import java.util.Calendar;
+import org.joda.time.*;
+
+//DateTime dt = new DateTime("2004-12-13T21:39:45.618-08:00");
 
 public class PizzaFactory {
 	
@@ -8,17 +10,17 @@ public class PizzaFactory {
 		
 	}
 	
-	public Pizza getPizza (Calendar dataPedido) {
-		switch (dataPedido.get(Calendar.DAY_OF_WEEK)) {
+	public Pizza getPizza (DateTime dataPedido) {
+		switch (dataPedido.getDayOfWeek()) {
+		case 1:
+		case 3:
+		case 5:
+			return new PizzaCalabresa();
 		case 2:
 		case 4:
 		case 6:
-			return new PizzaCalabresa();
-		case 3:
-		case 5:
-		case 7:
 			return new PizzaPresunto();	
-		case 1:
+		case 7:
 			System.out.println("Pizzaria fechada!");
 			return null;
 		default:
--- src/exercicio1_2/PizzaTests.java
+++ src/exercicio1_2/PizzaTests.java
@@ -2,7 +2,7 @@ package exercicio1_2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Calendar;
+import org.joda.time.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,29 +10,43 @@ import org.junit.jupiter.api.Test;
 class PizzaTests {
 	
 	private PizzaFactory factory;
-	private Calendar cal;
+	private DateTime data;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		factory = new PizzaFactory();
-		cal = Calendar.getInstance();
 	}
 	
 	@Test
 	void testPizzaNaQuartaFeira() {
-		cal.set(Calendar.DAY_OF_WEEK, 4);
-		System.out.println(cal.toString());
-		Pizza pizza = factory.getPizza(cal);
+		data = new DateTime("2018-10-10");
+		System.out.println("dia " + data.getDayOfMonth());
+		System.out.println("mês " + data.getMonthOfYear());
+		System.out.println("ano " + data.getYear());
+		Pizza pizza = factory.getPizza(data);
 		assertEquals("CALABRESA :: queijo + calabresa + tomate",
-				pizza.getIngredientes());
+				pizza.getIngredientes());			
 	}
 	
-//	@Test
-//	void testPizzaNaQuintaFeira() {
-//		Calendar.getInstance().set(;
-//		Pizza pizza = factory.getPizza(cal);
-//		assertEquals("PRESUNTO :: queijo + presunto + tomate",
-//				pizza.getIngredientes());
-//	}
+	@Test
+	void testPizzaNaQuintaFeira() {
+		data = new DateTime("2018-10-11");
+		System.out.println("dia " + data.getDayOfMonth());
+		System.out.println("mês " + data.getMonthOfYear());
+		System.out.println("ano " + data.getYear());
+		Pizza pizza = factory.getPizza(data);
+		assertEquals("PRESUNTO :: queijo + presunto + tomate",
+				pizza.getIngredientes());			
+	}
+	
+	@Test
+	void testRestauranteFechado() {
+		data = new DateTime("2018-10-14");
+		System.out.println("dia " + data.getDayOfMonth());
+		System.out.println("mês " + data.getMonthOfYear());
+		System.out.println("ano " + data.getYear());
+		Pizza pizza = factory.getPizza(data);
+		assertNull(pizza);			
+	}
 
 }
