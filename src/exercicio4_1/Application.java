88d99ecdc1297f1bc2eec6ab3945bd670f9bc5cf
Lucca Machado <lucca.santos@acad.pucrs.br>
2018-10-11 15:54:04 -0300

    exercício 4.1 funcionando :)

--- src/exercicio4_1/Application.java
+++ src/exercicio4_1/Application.java
@@ -1,13 +1,16 @@
 package exercicio4_1;
 
-public class Application {
-
-	public static void main(String[] args) {
-		Singleton sing = new Singleton();
-		
-		for (int i = 0; i < 10; i++) {
-			System.out.println(sing.getInstance().toString());
+public class Singleton {
+	
+	private Incremental incremental;
+	
+	public Incremental getInstance() {
+		if (incremental == null) {
+			incremental = new Incremental();
+			return incremental;
+		} else {
+			return incremental;
 		}
 	}
-
+	
 }
--- src/exercicio4_1/Incremental.java
+++ src/exercicio4_1/Incremental.java
@@ -1,20 +1,29 @@
 package exercicio4_1;
 
 public class Incremental {
-	
-	private static int count = 0;
-	private int numero;
-	
-	public Incremental() {
-		numero = ++count;
-	}
-	
+    
+    private static final Incremental instance = new Incremental();
+    
+    private static int count = 0;
+    private int numero;
+    
+    @Override
 	public String toString() {
-		return "Incremental " + numero;
-	}
-	
-	public int getNumero() {
-		return numero;
+		return "Incremental [numero=" + numero + "]";
 	}
 
+	private Incremental(){
+    	numero = ++count;
+    }
+
+    public static Incremental getInstance(){
+        return instance;
+    }
+    
+    public static void main(String[] args) {
+    		for (int i = 0; i < 10; i++) {
+    			Incremental inc = new Incremental();
+    			System.out.println(inc);
+    		}
+    }
 }
